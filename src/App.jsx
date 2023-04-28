0fb1dec543e0a054b34c527717b4fcbb697386e8
nurikgentle <nurikgentle@gmail.com>
2023-04-28 18:06:57 +0600

    all calculations are almost finished, but there are some bugs to fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,49 @@ import iconDollar from '../src/assets/icon-dollar.svg';
 import iconPerson from '../src/assets/icon-person.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bill, setBill] = useState(0);
+  const [people, setPeople] = useState(1);
+  const [custom, setCustom] = useState(0);
+  const [percentages, setPercentages] = useState([
+    {
+      id: 1,
+      percentage: 5,
+      isSelected: false
+    },
+    {
+      id: 2,
+      percentage: 10,
+      isSelected: false
+    },
+    {
+      id: 3,
+      percentage: 15,
+      isSelected: false
+    },
+    {
+      id: 4,
+      percentage: 25,
+      isSelected: false
+    },
+    {
+      id: 5,
+      percentage: 50,
+      isSelected: false
+    },
+  ]);
+
+  const resetEverything = () => {
+    setSelected(0)
+    setBill(0)
+    setPeople(0)
+    setCustom(0)
+    console.log('clicked');
+  }
+  console.log(percentages);
+  const [selected, setSelected] = useState(0);
+  const [isSelected, setIsSelected] = useState(null)
+  const tipAmount = (bill * (selected / 100)) / people
+  const total = parseInt(Number(bill) + Number(tipAmount))
 
   return (
     <div className="App">
@@ -18,29 +60,27 @@ function App() {
             <div className="bill-sec">
               <label for="bill">Bill</label>
               <div className="bill-input">
-                <input type="text" placeholder="0" id="bill" />
+                <input onChange={(e) => setBill(e.target.value)} type="number" placeholder="0" id="bill" />
                 <img src={iconDollar} alt="person" />
               </div>
             </div>
             <div className="tip-sec">
               <label for="tip">Select Tip %</label>
-              <div className="tip-btns">
+              <div className='tip-btns'>
                 <div className='tip-btn'>
-                  <button>5%</button>
-                  <button>10%</button>
-                  <button>15%</button>
-                </div>
-                <div className='tip-btn'>
-                  <button>25%</button>
-                  <button>50%</button>
-                  <input type="number" placeholder="Custom" id="custom" />
+                  {percentages.map((item, i) => {
+                    return (
+                      <button className={`${isSelected === item ? "isSelected" : null}`} key={item} onClick={(e) => { setSelected(item.percentage); setIsSelected(item) }}>{item.percentage}%</button>
+                    )
+                  })}
+                  <input value={custom} onChange={(e) => setCustom(e.target.value)} type="number" placeholder="Custom" id="custom" />
                 </div>
               </div>
             </div>
             <div class="people-sec">
               <label for="people">Number of People</label>
               <div className="people-input">
-                <input type="text" placeholder="0" id="people" />
+                <input value={people} onChange={(e) => setPeople(e.target.value)} type="text" placeholder="0" id="people" />
                 <img src={iconPerson} alt="person" />
               </div>
             </div>
@@ -48,13 +88,13 @@ function App() {
           <div className="outputs-section">
             <div className="tip-amount">
               <p>Tip Amount<br /><span>/ person</span></p>
-              <h2 id="tipAmount">$0.00</h2>
+              <h2 id="tipAmount">${parseInt(tipAmount / people)}</h2>
             </div>
             <div className="total">
               <p>Total<br /><span>/ person</span></p>
-              <h2 id="total">$0.00</h2>
+              <h2 id="total">${parseInt(total / people)}</h2>
             </div>
-            <button class="reset-btn" id="resetBtn">Reset</button>
+            <button onClick={resetEverything} class="reset-btn" id="resetBtn">Reset</button>
           </div>
         </div>
       </div>
