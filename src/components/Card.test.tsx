7fe16580e0f75d2a31d35cc6509e6002521fa7e2
nurikgentle <nurikgentle@gmail.com>
2023-04-30 23:29:43 +0600

    added some testing in Vitest

--- src/components/Card.test.tsx
+++ src/components/Card.test.tsx
@@ -1,8 +1,30 @@
-import { render, screen } from "@testing-library/react"
-import App from '../App'
+import {  render, screen } from "@testing-library/react";
+import Output from "./MiniCards/Output";
 
-it("should have hello world", () => {
-  render(<App />)
-  const message = screen.queryByText(/Hello World/i)
-  expect(message).toBeVisible()
-})
+describe("Props Output", () => {
+  test("total", () => {
+    const {} = render(
+      <Output
+        people={0}
+        resetEverything={() => {}}
+        tipAmount={0}
+        total={0}
+      ></Output>
+    );
+    const total = Number(screen.getByTestId("total").textContent);
+    expect(total).toEqual(NaN)
+  });
+
+  test("tipAmount", () => {
+    const {} = render(
+      <Output
+        people={0}
+        resetEverything={() => {}}
+        tipAmount={0}
+        total={0}
+      ></Output>
+    );
+    const tipAmount = Number(screen.getByTestId("tipAmount").textContent);
+    expect(tipAmount).toEqual(NaN)
+  });
+});
--- src/components/MiniCards/Output.tsx
+++ src/components/MiniCards/Output.tsx
@@ -20,7 +20,7 @@ const Output: React.FC<OutputProps> = ({
         <br />
         <span>/ person</span>
       </p>
-      <h2 id="tipAmount">${people > 0 ? (tipAmount / people).toFixed() : 0}</h2>
+      <h2 id="tipAmount" data-testid='tipAmount'>${people > 0 ? (tipAmount / people).toFixed() : 0}</h2>
     </div>
     <div className="total">
       <p>
@@ -28,7 +28,7 @@ const Output: React.FC<OutputProps> = ({
         <br />
         <span>/ person</span>
       </p>
-      <h2 id="total" data-testid='bill'>${total.toFixed()}</h2>
+      <h2 id="total" data-testid='total'>${total.toFixed()}</h2>
     </div>
     <button onClick={resetEverything} className="reset-btn" id="resetBtn">
       Reset
