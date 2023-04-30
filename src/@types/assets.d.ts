4f55608d3d288f66557ac2b41255bb959b1c8118
nurikgentle <nurikgentle@gmail.com>
2023-04-30 22:46:41 +0600

    have switched the jest to vitest, cuz understood that it is better for vite applcations

--- src/@types/assets.d.ts
+++ src/@types/assets.d.ts
@@ -1,4 +1,4 @@
 declare module '*.svg' {
-  const content: never;
+  const content: any;
   export default content;
 }
--- src/components/Card.test.tsx
+++ src/components/Card.test.tsx
@@ -1,24 +1,8 @@
-import { render } from "@testing-library/react";
-import Input from "./MiniCards/Input";
-import matchers from "@testing-library/jest-dom/matchers";
-expect.extend(matchers)
+import { render, screen } from "@testing-library/react"
+import App from '../App'
 
-describe(Input, () => {
-  it("Bill displays correct initial state", () => {
-    const { getByTestId } = render(
-      <Input
-        bill={0}
-        custom={0}
-        setPeople={0}
-        handleSelect={0}
-        isSelected={false}
-        people={0}
-        percentages={[]}
-        setBill={0}
-        handleCustom={0}
-      />
-    );
-    const billValue = Number(getByTestId("bill").textContent);
-    expect(billValue).toEqual(0);
-  });
-});
+it("should have hello world", () => {
+  render(<App />)
+  const message = screen.queryByText(/Hello World/i)
+  expect(message).toBeVisible()
+})
